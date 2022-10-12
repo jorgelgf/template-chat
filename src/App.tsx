e6117d0ad6d1fe1275394da47b10f59b2125df02
Jorge Luis <jlgurgelfernandes@gmail.com>
2022-10-12 16:58:52 -0300

    criado page Rout para rotas

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,12 @@ import Home from './pages/Home';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 import * as S from './styles'
-import HiBye from './pages/HiBye';
-import PDV from './pages/PDV';
-import Mobile from './pages/Mobile';
-import PC from './pages/PC';
-import Scanner from './pages/Scanner';
-import ImpR from './pages/Imp-Relatorio';
-import ImpEt from './pages/Imp-Etiqueta';
-import ImpCart from './pages/ImpCartao';
-import OnBoarding from './pages/OnBoarding';
-import Senha from './pages/Senha';
-import Webcam from './pages/Webcam';
-import Massivo from './pages/Massivo';
-import Locker from './pages/Locker';
-import Tv from './pages/Tv';
-import FilServer from './pages/FileServer';
+import Rout from './pages/Rout';
 
 function App() {
   return (
     <BrowserRouter>
-       <S.DivApp>
-       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/hibye' element={<HiBye/>}/>
-        <Route path='/pdv' element={<PDV/>}/>
-        <Route path='/mobile' element={<Mobile/>}/>
-        <Route path='/pc' element={<PC/>}/>
-        <Route path='/scanner' element={<Scanner/>}/>
-        <Route path='/ImpR' element={<ImpR/>}/>
-        <Route path='/impet' element={<ImpEt/>}/>
-        <Route path='/impcart' element={<ImpCart/>}/>
-        <Route path='/onboarding' element={<OnBoarding/>}/>
-        <Route path='/senha' element={<Senha/>}/>
-        <Route path='/webcam' element={<Webcam/>}/>
-        <Route path='/Massivo' element={<Massivo/>}/>
-        <Route path='/Locker' element={<Locker/>}/>
-        <Route path='/Tv' element={<Tv/>}/>
-        <Route path='/FilServer' element={<FilServer/>}/>
-
-
-
-
-       </Routes>
-
-       </S.DivApp>
+       <S.DivApp><Rout/></S.DivApp>
     </BrowserRouter>
   );
 }
---
+++ src/pages/Rout.tsx
@@ -0,0 +1,42 @@
+
+import { Routes,Route } from "react-router-dom";
+import Filserver from "./FileServer";
+import HiBye from "./HiBye";
+import Home from "./Home";
+import ImpEt from "./Imp-Etiqueta";
+import ImpR from "./Imp-Relatorio";
+import ImpCart from "./ImpCartao";
+import Locker from "./Locker";
+import Massivo from "./Massivo";
+import Mobile from "./Mobile";
+import OnBoarding from "./OnBoarding";
+import PC from "./PC";
+import PDV from "./PDV";
+import Scanner from "./Scanner";
+import Senha from "./Senha";
+import Tv from "./Tv";
+import Webcam from "./Webcam";
+
+export default function Rout (){
+
+    return<>
+     <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/hibye' element={<HiBye/>}/>
+        <Route path='/pdv' element={<PDV/>}/>
+        <Route path='/mobile' element={<Mobile/>}/>
+        <Route path='/pc' element={<PC/>}/>
+        <Route path='/scanner' element={<Scanner/>}/>
+        <Route path='/ImpR' element={<ImpR/>}/>
+        <Route path='/impet' element={<ImpEt/>}/>
+        <Route path='/impcart' element={<ImpCart/>}/>
+        <Route path='/onboarding' element={<OnBoarding/>}/>
+        <Route path='/senha' element={<Senha/>}/>
+        <Route path='/webcam' element={<Webcam/>}/>
+        <Route path='/Massivo' element={<Massivo/>}/>
+        <Route path='/Locker' element={<Locker/>}/>
+        <Route path='/Tv' element={<Tv/>}/>
+        <Route path='/FilServer' element={<Filserver/>}/>
+        </Routes>
+    </>
+}
