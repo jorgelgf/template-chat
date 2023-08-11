3880d1aac9534c86b72afa33d800cdc3e8fb86ff
Jorge Luis <jlgurgelfernandes@gmail.com>
2023-08-11 00:52:31 -0300

    adc

--- src/components/header/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-interface PropsHeader{
-    children: string
-}
-export const Header = ({children}:PropsHeader)=>{
-
-    return<S.HeaderContainer><header>{children}</header></S.HeaderContainer>
-    }
